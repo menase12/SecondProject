1dda667800e6039fb1fd07f6080831c12b492e8c
student <behabtuhiwot@gmail.com>
2017-10-02 12:32:18 -0400

    first commit

---
+++ src/hello_j.java
@@ -0,0 +1,12 @@
+public class hello_j {
+    public static void main(String arg [])
+    {/*these are the information the user asked to insert in*/
+        System.out.println("Hello my name is Yonatan Zewde. My level of experiance with java is Beginner.");
+        System.out.println("I graduated in Electrical Engineering from Morgan State University.");
+        System.out.println("I am intersted in playing a soccer, and I ma from Ethiopia.");
+
+    }
+
+
+
+}
